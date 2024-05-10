241cb6e30c760d89e16c5a39333644b8741cbceb
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2024-05-10 14:36:54 +0800

    Update GenerateAndConvert.py

--- GenerateAndConvert.py
+++ GenerateAndConvert.py
@@ -30,8 +30,7 @@ def main(source="./data", output="./AdGuard_Rule"):
             exportList = exportList + sourceDir[i]
         else:
             exportList = exportList + sourceDir[i] + ","
-    print("go run ./ --outputdir=" + output + " --exportlists=" + exportList)
-    os.system("go run ./ --outputdir=" + output + " --exportlists=" + exportList)
+    os.system("go run ./ --outputpath=" + output + " --exportlists=" + exportList)
     print("\n\n")
     for i in sourceDir:
         i = i.lower()
